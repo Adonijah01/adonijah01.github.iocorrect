71b9bf88e2720c068dafe579c7af5aba24dc6fee
adonijah01 <adonijahsingoei@gmail.com>
2025-03-24 11:13:31 +0300

    Cybernews

--- assets/js/certifications.js
+++ assets/js/certifications.js
@@ -1,21 +1,53 @@
 document.addEventListener("DOMContentLoaded", function () {
     const certContainer = document.getElementById("certifications");
 
-    // Manually add certifications (Credly does NOT allow public API access)
+    // Manually added certifications
     const certifications = [
         {
-            name: "Microsoft Certified: Security, Compliance, and Identity Fundamentals",
-            issuer: "Microsoft",
-            image: "https://images.credly.com/size/110x110/images/242902b5-f527-42ad-865e-977c9e1b5b58/image.png",
-            link: "https://www.credly.com/badges/YOUR_CERT_ID"
+            name: "CCNA: Introduction to Networks",
+            issuer: "Cisco",
+            image: "https://images.credly.com/size/80x80/images/70d71df5-f3dc-4380-9b9d-f22513a70417/CCNAITN__1_.png"
         },
         {
-            name: "AWS Certified Security – Specialty",
-            issuer: "Amazon Web Services",
-            image: "https://images.credly.com/size/110x110/images/ANOTHER_IMAGE_ID/image.png",
-            link: "https://www.credly.com/badges/ANOTHER_CERT_ID"
+            name: "Introduction to Cybersecurity",
+            issuer: "Cisco",
+            image: "https://images.credly.com/size/340x340/images/af8c6b4e-fc31-47c4-8dcb-eb7a2065dc5b/I2CS__1_.png"
+        },
+        {
+            name: "CCNA: Switching, Routing, and Wireless Essentials",
+            issuer: "Cisco",
+            image: "https://images.credly.com/size/80x80/images/f4ccdba9-dd65-4349-baad-8f05df116443/CCNASRWE__1_.png"
+        },
+        {
+            name: "CyberOps Associate",
+            issuer: "Cisco",
+            image: "https://images.credly.com/size/80x80/images/53f37f83-04a1-4935-9b1e-21a99cc6e1b2/CyberOpsAssoc.png"
+        },
+        {
+            name: "Network Security",
+            issuer: "Cisco",
+            image: "https://images.credly.com/size/64x64/images/f7387386-553c-4be5-b3f3-077f78152f31/Network_Security.png"
+        },
+        {
+            name: "Foundations of Operationalizing MITRE ATT&CK",
+            issuer: "MITRE",
+            image: "https://images.credly.com/size/64x64/images/466bf45d-c48d-46c3-a613-fd950d1dd9f9/Foundations_of_MITRE_ATT_CK_Badge.png"
+        },
+        {
+            name: "CCNA: Enterprise Networking, Security, and Automation",
+            issuer: "Cisco",
+            image: "https://images.credly.com/size/340x340/images/0a6d331e-8abf-4272-a949-33f754569a76/CCNAENSA__1_.png"
+        },
+        {
+            name: "AWS Academy Graduate - AWS",
+            issuer: "AWS",
+            image: "https://images.credly.com/size/64x64/images/73e4a58b-a8ef-41a3-a7db-9183dd269882/image.png"
+        },
+        {
+            name: "Ethical Hacker",
+            issuer: "EC-Council",
+            image: "https://images.credly.com/size/64x64/images/242902b5-f527-42ad-865e-977c9e1b5b58/image.png"
         }
-        // Add more certifications here
     ];
 
     if (certifications.length > 0) {
@@ -28,7 +60,6 @@ document.addEventListener("DOMContentLoaded", function () {
                     <div>
                         <h4>${cert.name}</h4>
                         <p>Issued by: ${cert.issuer}</p>
-                        <a href="${cert.link}" target="_blank">View Certification</a>
                     </div>
                 </div>
             `;
